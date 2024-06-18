c1fd37c718437cb592f173caa790d8f7e51a9a1c
kim-sun-a <gina305496@gmail.com>
2024-06-18 16:30:13 +0900

    index 수정

--- src/main/java/suna/myweb/controller/MainController.java
+++ src/main/java/suna/myweb/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
-public class TestController {
+public class MainController {
     @GetMapping("/")
     public String test(Model model) {
         model.addAttribute("data", "ok");
